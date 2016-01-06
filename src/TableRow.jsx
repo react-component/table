0c12d0c48f22fd60e116f2efc5ed439df882ca1c
绯一 <jaredlee.ljj@alipay.com>
2016-01-06 12:57:20 +0800

    [fix] onClick={null} if !onRowClick

--- src/TableRow.jsx
+++ src/TableRow.jsx
@@ -80,7 +80,7 @@ const TableRow = React.createClass({
       }
     }
     return (
-      <tr onClick={onRowClick.bind(this, record, index)} className={`${prefixCls} ${props.className}`} style={{display: props.visible ? '' : 'none'}}>{cells}</tr>);
+      <tr onClick={onRowClick ? onRowClick.bind(this, record, index) : null} className={`${prefixCls} ${props.className}`} style={{display: props.visible ? '' : 'none'}}>{cells}</tr>);
   },
 });
 
