e612443ec446b28454501ae8f7f93bb20db29560
afc163 <afc163@gmail.com>
2016-06-01 12:16:35 +0800

    fix lints (#47)

--- examples/nested.js
+++ examples/nested.js
@@ -1,45 +1,45 @@
-'use strict';
-
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Table = require('rc-table');
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Table = require('rc-table');
 require('rc-table/assets/index.less');
 
-var columns = [
+const columns = [
   {title: 'First Name', dataIndex: 'names.first', key: 'a', width: 100},
   {title: 'Last Name', dataIndex: 'names.last', key: 'b', width: 100},
   {title: 'Age', dataIndex: 'age', key: 'c', width: 100},
 ];
 
-var data = [{
+const data = [{
   age: '23',
   names: {
     first: 'John',
-    last: 'Doe'
+    last: 'Doe',
   },
-  key: '1'
+  key: '1',
 }, {
   age: '36',
   names: {
-    first: "Terry",
-    last: "Garner"
+    first: 'Terry',
+    last: 'Garner',
   },
-  key: '2'
+  key: '2',
 }, {
   age: '52',
   names: {
     first: 'Thomas',
-    last: 'Goodwin'
+    last: 'Goodwin',
   },
-  key: '3'
+  key: '3',
 }];
 
-var table = ReactDOM.render(
+ReactDOM.render(
   <div>
     <h2>Nested data table</h2>
-    <Table columns={columns}
+    <Table
+      columns={columns}
       data={data}
-      className="table"/>
+      className="table"
+    />
   </div>,
   document.getElementById('__react-content')
 );
