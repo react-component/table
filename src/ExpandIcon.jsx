5b0dc7a26d53f594a2a0b561158d90170214b5ef
afc163 <afc163@gmail.com>
2016-08-02 14:05:35 +0800

    Fix returned null in stateless function in react@0.14.x
    https://github.com/facebook/react/pull/5884

--- src/ExpandIcon.jsx
+++ src/ExpandIcon.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import shallowequal from 'shallowequal';
 
-export default ({ expandable, prefixCls, onExpand, needIndentSpaced, expanded, record }) => {
-  if (expandable) {
-    const expandClassName = expanded ? 'expanded' : 'collapsed';
-    return (
-      <span
-        className={`${prefixCls}-expand-icon ${prefixCls}-${expandClassName}`}
-        onClick={() => onExpand(!expanded, record)}
-      />
-    );
-  } else if (needIndentSpaced) {
-    return <span className={`${prefixCls}-expand-icon ${prefixCls}-spaced`} />;
-  }
-  return null;
-};
+const ExpandIcon = React.createClass({
+  propTypes: {
+    record: PropTypes.object,
+    prefixCls: PropTypes.string,
+    expandable: PropTypes.any,
+    expanded: PropTypes.bool,
+    needIndentSpaced: PropTypes.bool,
+    onExpand: PropTypes.func,
+  },
+  shouldComponentUpdate(nextProps) {
+    return !shallowequal(nextProps, this.props);
+  },
+  render() {
+    const { expandable, prefixCls, onExpand, needIndentSpaced, expanded, record } = this.props;
+    if (expandable) {
+      const expandClassName = expanded ? 'expanded' : 'collapsed';
+      return (
+        <span
+          className={`${prefixCls}-expand-icon ${prefixCls}-${expandClassName}`}
+          onClick={() => onExpand(!expanded, record)}
+        />
+      );
+    } else if (needIndentSpaced) {
+      return <span className={`${prefixCls}-expand-icon ${prefixCls}-spaced`} />;
+    }
+    return null;
+  },
+});
+
+export default ExpandIcon;
