f139be30431bf4c0685af76effa2ba7a02fd91ec
afc163 <afc163@gmail.com>
2016-08-02 14:06:57 +0800

    Updates

--- examples/common.js
+++ examples/common.js
@@ -23759,30 +23759,51 @@
 	
 	var _react2 = _interopRequireDefault(_react);
 	
+	var _shallowequal = __webpack_require__(189);
+	
+	var _shallowequal2 = _interopRequireDefault(_shallowequal);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	exports.default = function (_ref) {
-	  var expandable = _ref.expandable;
-	  var prefixCls = _ref.prefixCls;
-	  var onExpand = _ref.onExpand;
-	  var needIndentSpaced = _ref.needIndentSpaced;
-	  var expanded = _ref.expanded;
-	  var record = _ref.record;
+	var ExpandIcon = _react2.default.createClass({
+	  displayName: 'ExpandIcon',
 	
-	  if (expandable) {
-	    var expandClassName = expanded ? 'expanded' : 'collapsed';
-	    return _react2.default.createElement('span', {
-	      className: prefixCls + '-expand-icon ' + prefixCls + '-' + expandClassName,
-	      onClick: function onClick() {
-	        return onExpand(!expanded, record);
-	      }
-	    });
-	  } else if (needIndentSpaced) {
-	    return _react2.default.createElement('span', { className: prefixCls + '-expand-icon ' + prefixCls + '-spaced' });
+	  propTypes: {
+	    record: _react.PropTypes.object,
+	    prefixCls: _react.PropTypes.string,
+	    expandable: _react.PropTypes.any,
+	    expanded: _react.PropTypes.bool,
+	    needIndentSpaced: _react.PropTypes.bool,
+	    onExpand: _react.PropTypes.func
+	  },
+	  shouldComponentUpdate: function shouldComponentUpdate(nextProps) {
+	    return !(0, _shallowequal2.default)(nextProps, this.props);
+	  },
+	  render: function render() {
+	    var _props = this.props;
+	    var expandable = _props.expandable;
+	    var prefixCls = _props.prefixCls;
+	    var onExpand = _props.onExpand;
+	    var needIndentSpaced = _props.needIndentSpaced;
+	    var expanded = _props.expanded;
+	    var record = _props.record;
+	
+	    if (expandable) {
+	      var expandClassName = expanded ? 'expanded' : 'collapsed';
+	      return _react2.default.createElement('span', {
+	        className: prefixCls + '-expand-icon ' + prefixCls + '-' + expandClassName,
+	        onClick: function onClick() {
+	          return onExpand(!expanded, record);
+	        }
+	      });
+	    } else if (needIndentSpaced) {
+	      return _react2.default.createElement('span', { className: prefixCls + '-expand-icon ' + prefixCls + '-spaced' });
+	    }
+	    return null;
 	  }
-	  return null;
-	};
+	});
 	
+	exports.default = ExpandIcon;
 	module.exports = exports['default'];
 
 /***/ },
