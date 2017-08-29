a9aae22b0055b3990a60ba13bad2aec3253a0209
afc163 <afc163@gmail.com>
2017-08-29 17:49:21 +0800

    Updates

--- common.js
+++ common.js
@@ -14672,8 +14672,13 @@ var TableRow = function (_React$Component) {
       // Avoid displaying empty row
       // Fix https://github.com/ant-design/ant-design/issues/7379
       if (record.key === 'empty-placeholder-data') {
-        style.visibility = 'hidden';
-        style.pointEvents = 'none';
+        style.color = 'transparent';
+        style.background = 'transparent';
+        style.MozUserSelect = 'none';
+        style.WebkitUserSelect = 'none';
+        style.MsUserSelect = 'none';
+        style.userSelect = 'none';
+        style.pointerEvents = 'none';
       }
 
       return __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(
