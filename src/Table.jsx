81c1cec42b460f5ac04f83c7b67af3fdb229c1c5
afc163 <afc163@gmail.com>
2017-09-29 22:57:45 +0800

    fix vendor

--- src/Table.jsx
+++ src/Table.jsx
@@ -449,7 +449,7 @@ export default class Table extends React.Component {
       bodyStyle.overflowX = bodyStyle.overflowX || 'auto';
       // Fix weired webkit render bug
       // https://github.com/ant-design/ant-design/issues/7783
-      bodyStyle.webkitTransform = 'translate3d (0, 0, 0)';
+      bodyStyle.WebkitTransform = 'translate3d (0, 0, 0)';
     }
 
     const innerBodyStyle = {};
