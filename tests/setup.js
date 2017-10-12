a20ffc957ecebc943e5395bdf5ad701307bd9c57
afc163 <afc163@gmail.com>
2017-10-12 11:03:18 +0800

    fix eslint warning

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,4 @@
-global.requestAnimationFrame = global.requestAnimationFrame || function (cb) {
+global.requestAnimationFrame = global.requestAnimationFrame || function (cb) { // eslint-disable-line
   return setTimeout(cb, 0);
 };
 
