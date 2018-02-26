87d3bf683a098478c45302fdce3b1efdb05dbd15
hackape <muse.vincent@gmail.com>
2018-02-26 18:46:37 +0800

    copy HeadTable as FootTable, as a base to start

---
+++ src/FootTable.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { measureScrollbar } from './utils';
+import BaseTable from './BaseTable';
+
+export default function HeadTable(props, { table }) {
+  const { prefixCls, scroll, showHeader } = table.props;
+  const { columns, fixed, tableClassName, handleBodyScrollLeft, expander } = props;
+  const { saveRef } = table;
+  let { useFixedHeader } = table.props;
+  const headStyle = {};
+
+  if (scroll.y) {
+    useFixedHeader = true;
+    // Add negative margin bottom for scroll bar overflow bug
+    const scrollbarWidth = measureScrollbar();
+    if (scrollbarWidth > 0 && !fixed) {
+      headStyle.marginBottom = `-${scrollbarWidth}px`;
+      headStyle.paddingBottom = '0px';
+    }
+  }
+
+  if (!useFixedHeader || !showHeader) {
+    return null;
+  }
+
+  return (
+    <div
+      key="headTable"
+      ref={fixed ? null : saveRef('headTable')}
+      className={`${prefixCls}-header`}
+      style={headStyle}
+      onScroll={handleBodyScrollLeft}
+    >
+      <BaseTable
+        tableClassName={tableClassName}
+        hasHead
+        hasBody={false}
+        hasFoot={false}
+        fixed={fixed}
+        columns={columns}
+        expander={expander}
+      />
+    </div>
+  );
+}
+
+HeadTable.propTypes = {
+  fixed: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.bool,
+  ]),
+  columns: PropTypes.array.isRequired,
+  tableClassName: PropTypes.string.isRequired,
+  handleBodyScrollLeft: PropTypes.func.isRequired,
+  expander: PropTypes.object.isRequired,
+};
+
+HeadTable.contextTypes = {
+  table: PropTypes.any,
+};
