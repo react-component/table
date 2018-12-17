12335c2aab534eb8beaeb2223a90a35372b208dc
zombiej <smith3816@gmail.com>
2018-12-17 18:49:30 +0800

    fix scroll measure not work fine on ff of win fix ant-design/ant-design#13511

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 import warning from 'warning';
 
-let scrollbarSize;
+let scrollbarVerticalSize;
+let scrollbarHorizontalSize;
 
 // Measure scrollbar width for padding body during modal show/hide
 const scrollbarMeasure = {
@@ -8,31 +9,40 @@ const scrollbarMeasure = {
   top: '-9999px',
   width: '50px',
   height: '50px',
-  overflow: 'scroll',
 };
 
 export function measureScrollbar(direction = 'vertical') {
   if (typeof document === 'undefined' || typeof window === 'undefined') {
     return 0;
   }
-  if (scrollbarSize) {
-    return scrollbarSize;
+  const isVertical = direction === 'vertical';
+  if (isVertical && scrollbarVerticalSize) {
+    return scrollbarVerticalSize;
+  } else if (!isVertical && scrollbarHorizontalSize) {
+    return scrollbarHorizontalSize;
   }
   const scrollDiv = document.createElement('div');
   Object.keys(scrollbarMeasure).forEach(scrollProp => {
     scrollDiv.style[scrollProp] = scrollbarMeasure[scrollProp];
   });
+  // Append related overflow style
+  if (isVertical) {
+    scrollDiv.style.overflowY = 'scroll';
+  } else {
+    scrollDiv.style.overflowX = 'scroll';
+  }
   document.body.appendChild(scrollDiv);
   let size = 0;
-  if (direction === 'vertical') {
+  if (isVertical) {
     size = scrollDiv.offsetWidth - scrollDiv.clientWidth;
-  } else if (direction === 'horizontal') {
+    scrollbarVerticalSize = size;
+  } else if (!isVertical) {
     size = scrollDiv.offsetHeight - scrollDiv.clientHeight;
+    scrollbarHorizontalSize = size;
   }
 
   document.body.removeChild(scrollDiv);
-  scrollbarSize = size;
-  return scrollbarSize;
+  return size;
 }
 
 export function debounce(func, wait, immediate) {
