2efa58b8e5f7acfaa7ad4688098f56c406fab879
afc163 <afc163@gmail.com>
2019-10-18 11:46:05 +0800

    remove .storybook

--- .storybook/addons.js
+++
@@ -1,6 +0,0 @@
-
-import '@storybook/addon-actions/register';
-import '@storybook/addon-a11y/register';
-import '@storybook/addon-console';
-import '@storybook/addon-viewport/register';
-import 'storybook-addon-source/register';
