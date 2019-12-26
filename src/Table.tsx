react-component/table

commit 4aa8aa3a66b1b7c277a234b84c1bca2a06403ff5

:bug: Fix Table fixed columns height 1px bug
the last th of thead has border which make it 1px higher

close ant-design/ant-design#20126

src/Table.tsx
@@ -316,9 +316,8 @@ class Table<ValueType> extends React.Component<TableProps<ValueType>, TableState
       ? this.headTable.querySelectorAll('thead')
       : this.bodyTable.querySelectorAll('thead');
     const bodyRows = this.bodyTable.querySelectorAll(`.${prefixCls}-row`) || [];
-    const fixedColumnsHeadRowsHeight = [].map.call(
-      headRows,
-      (row: HTMLElement) => row.getBoundingClientRect().height || 'auto',
+    const fixedColumnsHeadRowsHeight = [].map.call(headRows, (row: HTMLElement) =>
+      row.getBoundingClientRect().height ? row.getBoundingClientRect().height - 1 : 'auto',
     );
     const state = this.store.getState();
     const fixedColumnsBodyRowsHeight = [].reduce.call(

commit 1129f4345a6475f4bb65b848cd84a424e4190b71

🐛 Fix Table fixed columns height 1px bug again
broken by 4aa8aa3

close ant-design/ant-design#20269

src/Table.tsx
@@ -317,7 +317,7 @@ class Table<ValueType> extends React.Component<TableProps<ValueType>, TableState
       : this.bodyTable.querySelectorAll('thead');
     const bodyRows = this.bodyTable.querySelectorAll(`.${prefixCls}-row`) || [];
     const fixedColumnsHeadRowsHeight = [].map.call(headRows, (row: HTMLElement) =>
-      row.getBoundingClientRect().height ? row.getBoundingClientRect().height - 1 : 'auto',
+      row.getBoundingClientRect().height ? row.getBoundingClientRect().height - 0.5 : 'auto',
     );
     const state = this.store.getState();
     const fixedColumnsBodyRowsHeight = [].reduce.call(