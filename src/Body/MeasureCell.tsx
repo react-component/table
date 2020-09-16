f594bf5620d8a2c3d7e282a2e38ac362a2254ecc
二货机器人 <smith3816@gmail.com>
2020-09-16 11:56:03 +0800

    fix: Default provides width even when no data (#530)
    * fix: Default provides width even when no data
    
    * update snapshot

--- src/Body/MeasureCell.tsx
+++ src/Body/MeasureCell.tsx
@@ -21,7 +21,9 @@ export default function MeasureCell({ columnKey, onColumnResize }: MeasureCellPr
         onColumnResize(columnKey, offsetWidth);
       }}
     >
-      <td ref={cellRef} style={{ padding: 0, border: 0, height: 0 }} />
+      <td ref={cellRef} style={{ padding: 0, border: 0, height: 0 }}>
+        <div style={{ height: 0, overflow: 'hidden' }}>&nbsp;</div>
+      </td>
     </ResizeObserver>
   );
 }
