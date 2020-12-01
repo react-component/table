ab15ba1d3ff03ddffcaf0e4856605c1e4fafc8bc
xrkffgg <xrkffgg@vip.qq.com>
2020-12-01 13:05:08 +0800

    lower dumi version

--- .umirc.ts
+++ .umirc.ts
@@ -8,5 +8,10 @@ export default defineConfig({
   logo:
     'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   outputPath: '.doc',
-  exportStatic: {}
+  exportStatic: {},
+  styles: [
+    `a img + svg {
+      display: none;
+    }`
+  ]
 });
