6690f1ca387774f43dfefbebe24046e27f238d4a
xrkffgg <xrkffgg@vip.qq.com>
2020-12-01 12:46:47 +0800

    update dumi version

--- .umirc.ts
+++ .umirc.ts
@@ -8,10 +8,5 @@ export default defineConfig({
   logo:
     'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   outputPath: '.doc',
-  exportStatic: {},
-  styles: [
-    `a img + svg {
-      display: none;
-    }`
-  ]
+  exportStatic: {}
 });
