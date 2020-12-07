1953bef9d3e9cecc2f83aa1682f2af9a341f526a
Hoikan <30694822+HoikanChan@users.noreply.github.com>
2020-12-07 23:18:07 +0800

    fix misspelling (#562)

--- src/hooks/useStickyOffsets.ts
+++ src/hooks/useStickyOffsets.ts
@@ -4,21 +4,21 @@ import { StickyOffsets } from '../interface';
 /**
  * Get sticky column offset width
  */
-function useStickyOffsets(colWidths: number[], columCount: number, direction: 'ltr' | 'rtl') {
+function useStickyOffsets(colWidths: number[], columnCount: number, direction: 'ltr' | 'rtl') {
   const stickyOffsets: StickyOffsets = useMemo(() => {
     const leftOffsets: number[] = [];
     const rightOffsets: number[] = [];
     let left = 0;
     let right = 0;
 
-    for (let start = 0; start < columCount; start += 1) {
+    for (let start = 0; start < columnCount; start += 1) {
       if (direction === 'rtl') {
         // Left offset
         rightOffsets[start] = right;
         right += colWidths[start] || 0;
 
         // Right offset
-        const end = columCount - start - 1;
+        const end = columnCount - start - 1;
         leftOffsets[end] = left;
         left += colWidths[end] || 0;
       } else {
@@ -27,7 +27,7 @@ function useStickyOffsets(colWidths: number[], columCount: number, direction: 'l
         left += colWidths[start] || 0;
 
         // Right offset
-        const end = columCount - start - 1;
+        const end = columnCount - start - 1;
         rightOffsets[end] = right;
         right += colWidths[end] || 0;
       }
@@ -37,7 +37,7 @@ function useStickyOffsets(colWidths: number[], columCount: number, direction: 'l
       left: leftOffsets,
       right: rightOffsets,
     };
-  }, [colWidths, columCount, direction]);
+  }, [colWidths, columnCount, direction]);
 
   return stickyOffsets;
 }
