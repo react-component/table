2f5e6a7c2d565a02d7f6b50e49311741ddfa4817
zombiej <smith3816@gmail.com>
2021-05-21 21:35:48 +0800

    chore: Cleanup useless code

--- src/Footer/index.tsx
+++ src/Footer/index.tsx
@@ -2,48 +2,27 @@ import * as React from 'react';
 import TableContext from '../context/TableContext';
 import Summary from './Summary';
 import type { ColumnType, StickyOffsets } from '../interface';
-import { getCellFixedInfo } from '../utils/fixUtil';
 
 type FlattenColumns<RecordType> = readonly (ColumnType<RecordType> & { scrollbar?: boolean })[];
 
-export const SummaryContext =
-  React.createContext<{
-    stickyOffsets?: StickyOffsets;
-    scrollColumnIndex?: number;
-    flattenColumns?: FlattenColumns<any>;
-  }>({});
+export const SummaryContext = React.createContext<{
+  stickyOffsets?: StickyOffsets;
+  scrollColumnIndex?: number;
+  flattenColumns?: FlattenColumns<any>;
+}>({});
 
 export interface FooterProps<RecordType> {
   children: React.ReactNode;
-  stickyOffsets?: StickyOffsets;
-  flattenColumns?: FlattenColumns<RecordType>;
+  stickyOffsets: StickyOffsets;
+  flattenColumns: FlattenColumns<RecordType>;
 }
 
-const EMPTY_LIST: FlattenColumns<any> = [];
-
-function Footer<RecordType>({
-  children,
-  stickyOffsets,
-  flattenColumns = EMPTY_LIST,
-}: FooterProps<RecordType>) {
+function Footer<RecordType>({ children, stickyOffsets, flattenColumns }: FooterProps<RecordType>) {
   const tableContext = React.useContext(TableContext);
-  const { prefixCls, direction } = tableContext;
+  const { prefixCls } = tableContext;
 
   const lastColumnIndex = flattenColumns.length - 1;
   const scrollColumn = flattenColumns[lastColumnIndex];
-
-  const mergedTableContext = React.useMemo(
-    () =>
-      stickyOffsets
-        ? {
-            ...tableContext,
-            fixedInfoList: flattenColumns.map((_, colIndex) =>
-              getCellFixedInfo(colIndex, colIndex, flattenColumns, stickyOffsets, direction),
-            ),
-          }
-        : tableContext,
-    [tableContext, stickyOffsets, flattenColumns, direction],
-  );
 
   const summaryContext = React.useMemo(
     () => ({
@@ -55,11 +34,9 @@ function Footer<RecordType>({
   );
 
   return (
-    <TableContext.Provider value={mergedTableContext}>
-      <SummaryContext.Provider value={summaryContext}>
-        <tfoot className={`${prefixCls}-summary`}>{children}</tfoot>
-      </SummaryContext.Provider>
-    </TableContext.Provider>
+    <SummaryContext.Provider value={summaryContext}>
+      <tfoot className={`${prefixCls}-summary`}>{children}</tfoot>
+    </SummaryContext.Provider>
   );
 }
 
