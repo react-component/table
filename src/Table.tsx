26a8454217685480eac6116a30c259c3f26958d7
Eric Han <hanfei.eric@bytedance.com>
2022-05-16 15:53:59 +0800

    fix: update col width when using cutomized body

--- src/Table.tsx
+++ src/Table.tsx
@@ -63,7 +63,7 @@ import useColumns from './hooks/useColumns';
 import { useLayoutState, useTimeoutLock } from './hooks/useFrame';
 import { getPathValue, mergeObject, validateValue, getColumnsKey } from './utils/valueUtil';
 import ResizeContext from './context/ResizeContext';
-import useStickyOffsets from './hooks/useStickyOffsets';
+import useStickyOffsets, { stickyOffsetCalculator } from './hooks/useStickyOffsets';
 import ColGroup from './ColGroup';
 import { getExpandableProps } from './utils/legacyUtil';
 import Panel from './Panel';
@@ -641,6 +641,11 @@ function Table<RecordType extends DefaultRecordType>(props: TableProps<RecordTyp
 
         return 0;
       }) as number[];
+      headerProps.stickyOffsets = stickyOffsetCalculator(
+        headerProps.colWidths,
+        flattenColumns.length,
+        direction,
+      );
     } else {
       bodyContent = (
         <div
