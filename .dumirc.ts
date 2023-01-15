69a184fba0bc5f1abed74bc89731a41618762314
kiner-tang(文辉) <1127031143@qq.com>
2023-01-15 15:03:07 +0800

    feat: update dumi version & config

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,9 +1,14 @@
-// more config: https://d.umijs.org/config
 import { defineConfig } from 'dumi';
+import path from 'path';
 
 export default defineConfig({
+  alias: {
+    'rc-table$': path.resolve('src'),
+    'rc-table/es': path.resolve('src'),
+  },
+  favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
   themeConfig: {
     name: 'Table',
+    logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
-  mfsu: false,
 });
