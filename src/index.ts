361a0b4a469681f23f44a57f06731e5c00f4be06
yoyo837 <yoyo837@hotmail.com>
2023-05-06 15:17:01 +0800

    feat: export INTERNAL_HOOKS directly

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { EXPAND_COLUMN } from './constant';
+import { EXPAND_COLUMN, INTERNAL_HOOKS } from './constant';
 import { FooterComponents as Summary } from './Footer';
 import Column from './sugar/Column';
 import ColumnGroup from './sugar/ColumnGroup';
@@ -6,6 +6,15 @@ import type { TableProps } from './Table';
 import Table, { genTable } from './Table';
 import { INTERNAL_COL_DEFINE } from './utils/legacyUtil';
 
-export { genTable, Summary, Column, ColumnGroup, TableProps, INTERNAL_COL_DEFINE, EXPAND_COLUMN };
+export {
+  genTable,
+  Summary,
+  Column,
+  ColumnGroup,
+  TableProps,
+  INTERNAL_COL_DEFINE,
+  EXPAND_COLUMN,
+  INTERNAL_HOOKS,
+};
 
 export default Table;
