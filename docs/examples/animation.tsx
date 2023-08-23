0b96098cd8dcbc05227e0e74f3ee3ce13fc3d2ba
叶枫 <645381995@qq.com>
2023-08-23 22:43:13 +0800

    feat: test

--- docs/examples/animation.tsx
+++ docs/examples/animation.tsx
@@ -1,114 +1,51 @@
-import React from 'react';
-import CSSMotionList from 'rc-animate/lib/CSSMotionList';
-import classNames from 'classnames';
-import toArray from 'rc-util/lib/Children/toArray';
 import Table from 'rc-table';
-import '../../assets/index.less';
-import './animation.less';
+import React, { useState } from 'react';
 
-type MotionBodyProps = React.HTMLAttributes<HTMLTableSectionElement>;
-
-const MotionBody: React.FC<MotionBodyProps> = ({ children, ...props }) => {
-  const nodeList = toArray(children);
-  const nodesRef = React.useRef<Record<React.Key, React.ReactElement>>({});
-
-  // Better apply clean up logic to avoid OOM
-  const keys: React.Key[] = [];
-  nodeList.forEach(node => {
-    const { key } = node;
-    nodesRef.current[key] = node;
-    keys.push(key);
-  });
-
+const EditableCell: React.FC<any> = ({ children, onOpen, _key, ...rest }) => {
+  console.log(111, _key);
   return (
-    <tbody {...props}>
-      <CSSMotionList keys={keys} component={false} motionName="move">
-        {({ key, className }) => {
-          const node = nodesRef.current[key];
-          return React.cloneElement(node, {
-            className: classNames(node.props.className, className),
-          });
-        }}
-      </CSSMotionList>
-    </tbody>
+    <td {...rest} onClick={() => onOpen(true)}>
+      {children}
+    </td>
+  );
+};
+export interface EditableCellProps extends React.HTMLAttributes<HTMLElement> {
+  children?: React.ReactNode;
+  open?: boolean;
+  onOpen: (open?: boolean) => void;
+}
+
+const App: React.FC = () => {
+  const [editKey, setEditKey] = useState('');
+  return (
+    <div>
+      <Table
+        components={{ body: { cell: EditableCell } }}
+        data={[
+          { key: '0', name: 'Edward King 0' },
+          { key: '1', name: 'Edward King 1' },
+          { key: '2', name: 'Edward King 2' },
+        ]}
+        columns={[
+          {
+            title: 'name',
+            dataIndex: 'name',
+            onCell: record => {
+              const thisKey = `${record.key}`;
+              return {
+                _key: record.key,
+                onOpen: () => {
+                  setEditKey(thisKey);
+                },
+                open: editKey === thisKey,
+              } as any;
+            },
+            shouldCellUpdate: () => false,
+          },
+        ]}
+      />
+    </div>
   );
 };
 
-interface RecordType {
-  a: string;
-  b?: string;
-  c?: string;
-  key: React.Key;
-}
-
-interface DemoState {
-  data: RecordType[];
-}
-
-class Demo extends React.Component<{}, DemoState> {
-  columns = [
-    { title: 'title1', dataIndex: 'a', key: 'a', width: 100 },
-    { id: '123', title: 'title2', dataIndex: 'b', key: 'b', width: 100 },
-    { title: 'title3', dataIndex: 'c', key: 'c', width: 200 },
-    {
-      title: 'Operations',
-      dataIndex: '',
-      key: 'd',
-      render: (text: string, record: RecordType) => (
-        <a onClick={e => this.onDelete(record.key, e)} href="#">
-          Delete
-        </a>
-      ),
-    },
-  ];
-
-  state: DemoState = {
-    data: [
-      { a: '123', key: '1' },
-      { a: 'cdd', b: 'edd', key: '2' },
-      { a: '1333', c: 'eee', key: '3' },
-    ],
-  };
-
-  onDelete = (key: React.Key, e: React.MouseEvent<HTMLElement>) => {
-    console.log('Delete', key);
-    e.preventDefault();
-    this.setState(({ data }) => ({
-      data: data.filter(item => item.key !== key),
-    }));
-  };
-
-  onAdd = () => {
-    this.setState(({ data }) => ({
-      data: [
-        ...data,
-        {
-          a: 'new data',
-          b: 'new data',
-          c: 'new data',
-          key: Date.now(),
-        },
-      ],
-    }));
-  };
-
-  render() {
-    return (
-      <div style={{ margin: 20 }}>
-        <h2>Table row with animation</h2>
-        <button type="button" onClick={this.onAdd}>
-          添加
-        </button>
-        <Table
-          columns={this.columns}
-          data={this.state.data}
-          components={{
-            body: { wrapper: MotionBody },
-          }}
-        />
-      </div>
-    );
-  }
-}
-
-export default Demo;
+export default App;
