a6dcf12c3112c8d5c4e9497dc105569d3a0c300a
madocto <hi.madocto@gmail.com>
2024-01-19 18:43:33 +0800

    feat: add demo

---
+++ docs/demo/disabled-hover.md
@@ -0,0 +1,8 @@
+---
+title: disabled-hover
+nav:
+  title: Demo
+  path: /demo
+---
+
+<code src="../examples/disabled-hover.tsx"></code>
---
+++ docs/examples/disabled-hover.tsx
@@ -0,0 +1,23 @@
+import type { TableProps } from 'rc-table';
+import Table from 'rc-table';
+import React from 'react';
+import '../../assets/index.less';
+
+interface RecordType {
+  a?: string;
+}
+
+const data = [{ a: 'A' }, { a: 'B' }, { a: 'C' }];
+
+const Demo = () => {
+  const columns: TableProps<any>['columns'] = [
+    {
+      title: 'title',
+      dataIndex: 'a',
+    },
+  ];
+
+  return <Table<RecordType> columns={columns} data={data} disabledHover />;
+};
+
+export default Demo;
