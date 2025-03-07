8af54985070007f66764ae89f55fe823ddff3ea5
二货机器人 <smith3816@gmail.com>
2025-03-07 11:46:48 +0800

    test: mv Colgroup.spec.jsx

--- tests/Colgroup.spec.jsx
+++ tests/Colgroup.spec.jsx
@@ -1,6 +1,7 @@
-import { mount } from 'enzyme';
 import React from 'react';
+import { render } from '@testing-library/react';
 import Table, { INTERNAL_COL_DEFINE } from '../src';
+import Cell from '../src/Cell';
 
 describe('Table.ColGroup', () => {
   it('internal props should render', () => {
@@ -10,21 +11,62 @@ describe('Table.ColGroup', () => {
         [INTERNAL_COL_DEFINE]: { className: 'show-in-col' },
       },
     ];
-
-    const wrapper = mount(<Table columns={columns} />);
-    expect(wrapper.find('colgroup col').props().className).toEqual('show-in-col');
+    const { container } = render(<Table columns={columns} />);
+    const colEl = container.querySelector('colgroup col');
+    expect(colEl?.className).toEqual('show-in-col');
   });
 
   it('correct key', () => {
-    const columns = [
-      {
-        key: 0,
-        width: 1,
-      },
-    ];
+    const column1 = {
+      key: 'bamboo',
+      width: 1,
+    };
 
-    const wrapper = mount(<Table columns={columns} />);
-    expect(String(wrapper.find('colgroup col').key())).toEqual('0');
+    const column2 = {
+      key: 'little',
+      width: 1,
+    };
+
+    const column3 = {
+      key: 'light',
+      width: 1,
+    };
+
+    let unmount = 0;
+
+    const ProxyCell = props => {
+      React.useEffect(() => {
+        return () => {
+          unmount += 1;
+        };
+      }, []);
+
+      return <th {...props} />;
+    };
+
+    const { rerender } = render(
+      <Table
+        columns={[column1, column2]}
+        components={{
+          header: {
+            cell: ProxyCell,
+          },
+        }}
+      />,
+    );
+
+    rerender(
+      <Table
+        columns={[column2, column3]}
+        components={{
+          header: {
+            cell: ProxyCell,
+          },
+        }}
+      />,
+    );
+
+    expect(unmount).toEqual(1);
   });
 
   it('minWidth should be worked', () => {
@@ -34,9 +76,9 @@ describe('Table.ColGroup', () => {
         minWidth: 100,
       },
     ];
-
-    const wrapper = mount(<Table columns={columns} />);
-    expect(wrapper.find('colgroup col').at(0).props().style.minWidth).toEqual(100);
+    const { container } = render(<Table columns={columns} />);
+    const colEl = container.querySelector('colgroup col');
+    expect(colEl?.style.minWidth).toEqual('100px');
   });
 
   it('should not have minWidth when tableLayout is fixed', () => {
@@ -47,8 +89,8 @@ describe('Table.ColGroup', () => {
         minWidth: 100,
       },
     ];
-
-    const wrapper = mount(<Table columns={columns} tableLayout="fixed" />);
-    expect(wrapper.find('colgroup col').at(0).props().style.minWidth).toBeFalsy();
+    const { container } = render(<Table columns={columns} tableLayout="fixed" />);
+    const colEl = container.querySelector('colgroup col');
+    expect(colEl?.style.minWidth).toBeFalsy();
   });
 });
