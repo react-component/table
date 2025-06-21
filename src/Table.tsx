c6d817ebd87ce5270dbd24a986b32c8853800be1
lijianan <574980606@qq.com>
2025-06-21 21:22:26 +0800

    Update Table.tsx

--- src/Table.tsx
+++ src/Table.tsx
@@ -907,7 +907,6 @@ function Table<RecordType extends DefaultRecordType>(
       fullTableRef,
       colsWidths,
       colsKeys,
-      colWidths,
       onColumnResizeEnd,
       onResizingChange: setIsResizing,
     }),
