ab7bcad66c3174078f7dc50643362a3b3fd9bef3
lijianan <574980606@qq.com>
2025-09-15 11:32:17 +0800

    update

--- docs/examples/measureRowRender.tsx
+++ docs/examples/measureRowRender.tsx
@@ -30,12 +30,15 @@ const data = [
   { key: 3, name: 'Jime', age: 35 },
 ];
 
-// 注意，这个 measureRow 实际上是一个 <tr> 标签
-// 按照 html 规范，tr 的父元素必须是 table、thead、tbody、tfoot 标签，子元素必须是 th、td 标签
-// 因此这里我们用一个 div 包裹是不对的，在控制台中会报错
-const measureRowRender: TableProps['measureRowRender'] = measureRow => (
-  <div style={{ display: 'none' }}>{measureRow}</div>
-);
+// 自定义 MeasureRow 渲染，隐藏弹层内容
+const measureRowRender: TableProps['measureRowRender'] = measureRow => {
+  if (React.isValidElement(measureRow)) {
+    return React.cloneElement<any>(measureRow, {
+      style: { ...(measureRow.props as any).style, display: 'none' },
+    });
+  }
+  return measureRow;
+};
 
 // 示例：使用 measureRowRender 来隐藏 MeasureRow 中的弹层
 const MeasureRowRenderExample: React.FC = () => {
