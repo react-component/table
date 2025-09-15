3efc9434fd1c4ffe8a9136b8edfcff49af43d38e
lijianan <574980606@qq.com>
2025-09-15 12:00:40 +0800

    fix: avoiding closure problems
    avoiding closure problems

--- docs/examples/measureRowRender.tsx
+++ docs/examples/measureRowRender.tsx
@@ -31,9 +31,14 @@ const data = [
 ];
 
 // 自定义 MeasureRow 渲染，隐藏弹层内容
-const measureRowRender: TableProps['measureRowRender'] = measureRow => (
-  <div style={{ display: 'none' }}>{measureRow}</div>
-);
+const measureRowRender: TableProps['measureRowRender'] = measureRow => {
+  if (React.isValidElement(measureRow)) {
+    return React.cloneElement<any>(measureRow, {
+      style: { ...(measureRow.props as any).style, display: 'none' },
+    });
+  }
+  return measureRow;
+};
 
 // 示例：使用 measureRowRender 来隐藏 MeasureRow 中的弹层
 const MeasureRowRenderExample: React.FC = () => {
