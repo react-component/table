f9809b7a6bd970b449410af128f042f49b097ce6
afc163 <afc163@gmail.com>
2025-09-22 19:15:51 +0800

    fix: Table MeasureRow bug when children have refs (#1367)
    * fix: Table MeasureRow bug when children have refs
    
    * refactor: remove unused ColumnType import in MeasureCell.tsx
    
    ---------
    
    Co-authored-by: keda <53565274+Linkodt@users.noreply.github.com>

--- src/Body/MeasureCell.tsx
+++ src/Body/MeasureCell.tsx
@@ -1,20 +1,19 @@
 import * as React from 'react';
 import ResizeObserver from 'rc-resize-observer';
 import useLayoutEffect from 'rc-util/lib/hooks/useLayoutEffect';
-import type { ColumnType } from '../interface';
 
 export interface MeasureCellProps {
   columnKey: React.Key;
   onColumnResize: (key: React.Key, width: number) => void;
-  column?: ColumnType<any>;
   prefixCls: string;
+  title?: React.ReactNode;
 }
 
 export default function MeasureCell({
   columnKey,
   onColumnResize,
-  column,
   prefixCls,
+  title,
 }: MeasureCellProps) {
   const cellRef = React.useRef<HTMLTableCellElement>();
 
@@ -27,7 +26,7 @@ export default function MeasureCell({
   return (
     <ResizeObserver data={columnKey}>
       <th ref={cellRef} className={`${prefixCls}-measure-cell`}>
-        <div className={`${prefixCls}-measure-cell-content`}>{column?.title || '\xa0'}</div>
+        <div className={`${prefixCls}-measure-cell-content`}>{title || '\xa0'}</div>
       </th>
     </ResizeObserver>
   );
