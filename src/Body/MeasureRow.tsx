55866d18565042d9e4362df847a7b87f0581a6b3
afc163 <afc163@gmail.com>
2025-10-09 19:07:56 +0800

    Revert "fix: duplicate unique identifiers in MeasureRow column headers (#1376)"
    This reverts commit c58ee99b3a3a96e4a59ad40f4cd3a710d40f455b.

--- src/Body/MeasureRow.tsx
+++ src/Body/MeasureRow.tsx
@@ -5,7 +5,6 @@ import isVisible from '@rc-component/util/lib/Dom/isVisible';
 import { useContext } from '@rc-component/context';
 import TableContext from '../context/TableContext';
 import type { ColumnType } from '../interface';
-import { prepareMeasureTitle } from '../utils/measureUtil';
 
 export interface MeasureRowProps {
   prefixCls: string;
@@ -38,8 +37,9 @@ const MeasureRow: React.FC<MeasureRowProps> = ({
         {columnsKey.map(columnKey => {
           const column = columns.find(col => col.key === columnKey);
           const rawTitle = column?.title;
-          const titleForMeasure = prepareMeasureTitle(rawTitle);
-
+          const titleForMeasure = React.isValidElement<React.RefAttributes<any>>(rawTitle)
+            ? React.cloneElement(rawTitle, { ref: null })
+            : rawTitle;
           return (
             <MeasureCell
               key={columnKey}
