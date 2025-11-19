70e42aa88406114cc46542361ea885320747dcc4
FatahChan <ahmadfathallah89@gmail.com>
2025-11-19 23:58:46 +0200

    refactor: convert Demo component to functional component and optimize column resizing logic

--- docs/examples/column-resize.tsx
+++ docs/examples/column-resize.tsx
@@ -1,95 +1,71 @@
-import React from 'react';
-import { Resizable } from 'react-resizable';
+import React, { useMemo, useState } from 'react';
+import type { ColumnType, ColumnsType } from 'rc-table';
 import Table from 'rc-table';
+import { Resizable } from 'react-resizable';
 import '../../assets/index.less';
 import 'react-resizable/css/styles.css';
-import type { ColumnType } from '@/interface';
-
 const ResizableTitle = props => {
   const { onResize, width, ...restProps } = props;
 
   if (!width) {
     return <th {...restProps} />;
   }
-
   return (
-    <Resizable width={width} height={0} onResize={onResize}>
+    <Resizable width={width} onResize={onResize}>
       <th {...restProps} />
     </Resizable>
   );
 };
 
-interface RecordType {
-  a: string;
-  b?: string;
-  c?: string;
-  d?: number;
-  key: string;
-}
+const data = [
+  { a: '123', key: '1' },
+  { a: 'cdd', b: 'edd', key: '2' },
+  { a: '1333', c: 'eee', d: 2, key: '3' },
+] as const;
 
-interface DemoState {
-  columns: ColumnType<RecordType>[];
-}
+type RecordType = (typeof data)[number];
 
-class Demo extends React.Component<{}, DemoState> {
-  state: DemoState = {
-    columns: [
-      { title: 'title1', dataIndex: 'a', key: 'a', width: 100 },
-      { title: 'title2', dataIndex: 'b', key: 'b', width: 100 },
-      { title: 'title3', dataIndex: 'c', key: 'c', width: 200 },
-      {
-        title: 'Operations',
-        dataIndex: '',
-        key: 'd',
-        render() {
-          return <a href="#">Operations</a>;
-        },
+const Demo = () => {
+  const [columns, setColumns] = useState<ColumnsType<RecordType>>([
+    { title: 'title1', dataIndex: 'a', key: 'a', width: 100 },
+    { title: 'title2', dataIndex: 'b', key: 'b', width: 100 },
+    { title: 'title3', dataIndex: 'c', key: 'c', width: 200 },
+    {
+      title: 'Operations',
+      dataIndex: '',
+      key: 'd',
+      render() {
+        return <a href="#">Operations</a>;
       },
-    ],
-  };
-
-  components = {
-    header: {
-      cell: ResizableTitle,
     },
-  };
-
-  data = [
-    { a: '123', key: '1' },
-    { a: 'cdd', b: 'edd', key: '2' },
-    { a: '1333', c: 'eee', d: 2, key: '3' },
-  ];
-
-  handleResize =
-    index =>
-    (e, { size }) => {
-      this.setState(({ columns }) => {
-        const nextColumns = [...columns];
-        nextColumns[index] = {
-          ...nextColumns[index],
-          width: size.width,
-        };
-        return { columns: nextColumns };
-      });
+  ]);
+  const handleResize = useMemo(() => {
+    return (index: number) => {
+      return (_: React.MouseEvent<HTMLTableCellElement>, { size }: { size: { width: number } }) => {
+        setColumns(prevColumns =>
+          prevColumns.map((col, i) => (i === index ? { ...col, width: size.width } : col)),
+        );
+      };
     };
-
-  render() {
-    const columns = this.state.columns.map((col, index) => ({
+  }, []);
+  const columnsWithResizable = useMemo(() => {
+    return columns.map((col, index) => ({
       ...col,
-      onHeaderCell: (column: ColumnType<RecordType>) =>
-        ({
-          width: column.width,
-          onResize: this.handleResize(index),
-        }) as any,
+      onHeaderCell: (column: ColumnType<RecordType>) => ({
+        width: column.width,
+        onResize: handleResize(index),
+      }),
     }));
+  }, [columns, handleResize]);
 
-    return (
-      <div>
-        <h2>Integrate with react-resizable</h2>
-        <Table components={this.components} columns={columns} data={this.data} />
-      </div>
-    );
-  }
-}
+  const tableProps = useMemo(() => {
+    return {
+      components: { header: { cell: ResizableTitle } },
+      columns: columnsWithResizable,
+      data,
+    };
+  }, [columnsWithResizable]);
+  return <Table {...tableProps} />;
+};
 
 export default Demo;
